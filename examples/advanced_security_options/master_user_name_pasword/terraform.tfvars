13aa6eb7d21b0399ecefb12fb8dc5af02706f0d1
Michael Heitland (ext.) <Michael.Heitland@partner.bmwgroup.com>
2020-08-26 17:41:47 +0200

    removing sample module, splitting pipeline stages

--- examples/advanced_security_options/master_user_name_pasword/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-region         = "eu-central-1"
-profile        = "default"
-es_domain_name = "elasticsearch-public"
-es_version     = "7.1"
-whitelist      = ["1.1.1.1", "2.2.2.2"]
--- examples/vpc/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {}
-variable "profile" {}
-
-# AWS Elasticsearch
-variable "es_domain_name" {}
-variable "es_version" {}
-variable "subnet_ids" {}
-variable "security_group_ids" {}
